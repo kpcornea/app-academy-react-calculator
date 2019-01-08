88184be7946d84429cae27f63e89c9f701bde1b8
Kevin Cornea <kpcornea@gmail.com>
2019-01-08 00:52:26 -0800

    Add calculator component

--- app.jsx
+++ app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './hello';
+import Calculator from './calculator';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Hello/>, root);
+  ReactDOM.render(<Calculator/>, root);
 });
