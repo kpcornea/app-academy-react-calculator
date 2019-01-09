320db98329f386916747e9f683a0e3f09791b8b2
Kevin Cornea <kpcornea@gmail.com>
2019-01-08 20:33:11 -0800

    Complete basic functionality

--- calculator.jsx
+++ calculator.jsx
@@ -12,7 +12,11 @@ class Calculator extends React.Component {
 
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
-    this.setResult = this.setResult.bind(this);
+    this.clear = this.clear.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
   }
 
   setNum1(event) {
@@ -23,8 +27,30 @@ class Calculator extends React.Component {
     this.setState({ num2: event.target.value });
   }
 
-  setResult(event) { // this is just adding right now
+  clear(event) {
+    this.setState({ num1: "", num2: "", result: 0 });
+    event.preventDefault();
+  }
+
+  add(event) {
     this.setState({ result: Number(this.state.num1) + Number(this.state.num2) });
+    event.preventDefault();
+  }
+
+  subtract(event) {
+    this.setState({ result: Number(this.state.num1) - Number(this.state.num2) });
+    event.preventDefault();
+  }
+
+  multiply(event) {
+    this.setState({ result: Number(this.state.num1) * Number(this.state.num2) });
+    event.preventDefault();
+  }
+
+// when to prevent default?
+
+  divide(event) {
+    this.setState({ result: Number(this.state.num1) / Number(this.state.num2) });
     event.preventDefault();
   }
 
@@ -33,16 +59,15 @@ class Calculator extends React.Component {
       <div>
         <h1>{this.state.result}</h1>
         <br />
-        <form onSubmit={this.setResult}>
-          <label>
-            Num1:
-            <input type="text" value={this.state.num1} onChange={this.setNum1} />
-          </label>
-          <label>
-            Num2:
-            <input type="text" value={this.state.num2} onChange={this.setNum2} />
-          </label>
-          <input type="submit" value="Add" />
+        <form>
+          <input type="text" value={this.state.num1} onChange={this.setNum1} />
+          <input type="text" value={this.state.num2} onChange={this.setNum2} />
+          <button onClick={this.clear}>Clear</button>
+          <br />
+          <button onClick={this.add}>+</button>
+          <button onClick={this.subtract}>-</button>
+          <button onClick={this.multiply}>*</button>
+          <button onClick={this.divide}>/</button>
         </form>
       </div>
     );
