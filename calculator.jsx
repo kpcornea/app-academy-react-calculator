d798fa58cf4f925a76d47aec2dbc81efb44f2a8d
Kevin Cornea <kpcornea@gmail.com>
2019-01-08 19:19:14 -0800

    Complete add version

--- calculator.jsx
+++ calculator.jsx
@@ -11,20 +11,21 @@ class Calculator extends React.Component {
     };
 
     this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.setResult = this.setResult.bind(this);
   }
 
   setNum1(event) {
-    // e.preventDefault();
-    this.setState({ num1: event.target.value })
+    this.setState({ num1: event.target.value });
   }
 
   setNum2(event) {
-    // e.preventDefault();
-    this.setState({ num2: event.target.value })
+    this.setState({ num2: event.target.value });
   }
 
   setResult(event) { // this is just adding right now
-    this.setState({ result: Number(this.state.num1) + Number(this.state.num2) })
+    this.setState({ result: Number(this.state.num1) + Number(this.state.num2) });
+    event.preventDefault();
   }
 
   render() {
@@ -32,11 +33,16 @@ class Calculator extends React.Component {
       <div>
         <h1>{this.state.result}</h1>
         <br />
-        <form>
+        <form onSubmit={this.setResult}>
           <label>
-          Num1:
-          <input type="text" value={this.state.num1} onChange={this.setNum1}
+            Num1:
+            <input type="text" value={this.state.num1} onChange={this.setNum1} />
           </label>
+          <label>
+            Num2:
+            <input type="text" value={this.state.num2} onChange={this.setNum2} />
+          </label>
+          <input type="submit" value="Add" />
         </form>
       </div>
     );
