89c4a64c19f60e112830edd37c5ce5df8ad9d586
Kevin Cornea <kpcornea@gmail.com>
2019-01-08 19:06:20 -0800

    Add wip change state functions

--- calculator.jsx
+++ calculator.jsx
@@ -9,14 +9,35 @@ class Calculator extends React.Component {
         num2: "",
         result: 0
     };
+
+    this.setNum1 = this.setNum1.bind(this);
   }
 
-  // your code here
+  setNum1(event) {
+    // e.preventDefault();
+    this.setState({ num1: event.target.value })
+  }
+
+  setNum2(event) {
+    // e.preventDefault();
+    this.setState({ num2: event.target.value })
+  }
+
+  setResult(event) { // this is just adding right now
+    this.setState({ result: Number(this.state.num1) + Number(this.state.num2) })
+  }
 
   render() {
     return (
       <div>
         <h1>{this.state.result}</h1>
+        <br />
+        <form>
+          <label>
+          Num1:
+          <input type="text" value={this.state.num1} onChange={this.setNum1}
+          </label>
+        </form>
       </div>
     );
   }
